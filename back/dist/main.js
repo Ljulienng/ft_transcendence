53f529bb7e377e3d82de7886c1ff937500bffb5f
eteillet <elie.teillet@gmail.com>
2022-04-21 15:51:18 +0200

    add serve-static dependencies for chat

--- back/dist/main.js
+++ back/dist/main.js
@@ -4,9 +4,11 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const cookieParser = require("cookie-parser");
 const passport = require("passport");
+const path_1 = require("path");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.use(passport.initialize());
+    app.useStaticAssets((0, path_1.join)(__dirname, '..', 'static'));
     app.use(cookieParser());
     app.enableCors({
         credentials: true,
