00087965694ba3cf71788f2c1d48690e01c643d8
agautier <pillowin@gmx.com>
2022-05-03 19:40:31 +0200

    Merge branch 'master' into agautier

---
+++ front/src/router/index.ts
@@ -0,0 +1,87 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
+import HelloWorld from '@/components/HelloWorld.vue'
+import ShowUsers from '@/components/ShowUsers.vue'
+import Pong from '@/components/Pong.vue'
+import AuthModal from '@/components/AuthModal.vue'
+import FriendList from '@/views/FriendList.vue'
+import Home from '@/views/Home.vue'
+
+
+const routes = [
+	{
+		name: 'Home',
+		path: '/home',
+		component: Home,
+		meta: {requiredAuth: true}
+	},
+	{
+		name: 'Hello',
+		path: '/hello',
+		component: HelloWorld
+	},
+	{
+		name: 'ShowUsers',
+		path: '/showusers',
+		component: ShowUsers
+	},
+	{
+		name: 'Play',
+		path: '/play',
+		component: Pong
+	},
+	{
+		name: 'AuthModal',
+		path: '/authmodal',
+		component: AuthModal
+	},
+	{
+		name: 'FriendList',
+		path: '/friendlist',
+		component: FriendList,
+		meta: {requiredAuth: true}
+	}
+];
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes,
+})
+
+<<<<<<< HEAD
+export default router;
+=======
+router.beforeEach(async (to, from, next) => {
+	if (to.meta.requiredAuth) {
+	let userProfile = store.getters["auth/getUserProfile"];
+		if (userProfile.id === 0) {
+			await store.dispatch("auth/userProfile");
+			userProfile = store.getters["auth/getUserProfile"];
+			if (userProfile.id === 0) {
+				return next({ path: "/authmodal" });
+			} else {
+				return next();
+			}
+		}
+	}
+	return next();
+});
+
+// router.beforeEach(async (to, from, next) => {
+// 	if (to.) {
+// 	let userProfile = store.getters["auth/getUserProfile"];
+// 		if (userProfile.id === 0) {
+// 			await store.dispatch("auth/userProfile");
+// 			userProfile = store.getters["auth/getUserProfile"];
+// 			if (userProfile.id === 0) {
+// 				return next({ path: "/authmodal" });
+// 			} else {
+// 				return next();
+// 			}
+// 		}
+// 	}
+// 	return next();
+// });
+
+export default router;
+>>>>>>> master
