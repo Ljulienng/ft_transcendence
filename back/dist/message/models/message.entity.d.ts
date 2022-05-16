9223c1aa6466b35a834da281f8ec0d9e813252ed
agautier <agautier@student.42.fr>
2022-05-16 16:34:13 +0200

    fix: update gitignore

--- back/dist/message/models/message.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { Channel } from "src/channel/models/channel.entity";
-import { User } from 'src/user/models/user.entity';
-export declare class Message {
-    readonly id: number;
-    content: string;
-    readonly createdTime: Date;
-    user: User;
-    channel: Channel;
-}
