cc87a76c831c313e063f189a0380e3211d063410
Ljulienng <ljuliennguyen@gmail.com>
2022-05-21 04:07:07 +0200

    chat is perfect

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -18,8 +18,8 @@ import { ChannelMemberModule } from './channelMember/channelMember.module';
     ConfigModule.forRoot({isGlobal: true}),
     TypeOrmModule.forRoot({ // for PostGres 
       type: 'postgres',
-      // url: process.env.DATABASE_URL,
-      url: "postgres://user:password@localhost:5432/db",
+      url: process.env.DATABASE_URL,
+      // url: "postgres://user:password@localhost:5432/db",
       autoLoadEntities: true,
       synchronize: true // false for production, with true typeOrm creates tables every time we run the app
     }),
