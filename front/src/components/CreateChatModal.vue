a5383b6acd9513aad511f97e9aa223efa533865c
Marie-clemence CIUPEK <mciupek@e2r9p1.clusters.42paris.fr>
2022-05-30 10:42:08 +0200

    front(chat): create chat modal

---
+++ front/src/components/CreateChatModal.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+
+    <b-modal id="modal-1" title="BootstrapVue">
+        <p class="my-4">Hello from modal!</p>
+    </b-modal>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from "@vue/runtime-core"
+
+export default defineComponent({
+    // setup() {
+        
+    // },
+})
+</script>
+
+<style scoped>
+
+</style>
