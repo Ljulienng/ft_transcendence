bc9a746076369e37403e23f1d2bcddcf47573eb8
Marie-clemence CIUPEK <mciupek@e2r9p1.clusters.42paris.fr>
2022-05-30 15:42:28 +0200

    front(friendlist): friends list view

--- front/src/views/FriendList.vue
+++ front/src/views/FriendList.vue
@@ -1,35 +1,77 @@
 <template>
+
 	<div id="friend">
 
-		<form v-on:submit.prevent="addFriend">
-			<p>
-				<label for="id">friend username </label>
-				<input
-				id="id"
-				v-model="friendToAdd.friendUsername"
-				type="string"
-				name="id"
-				>
-			</p>
-			<p v-if="errorMsg !== ''" style="color: red">
-				{{errorMsg}}
-			</p>
-			<p>
-				<input
-				type="submit"
-				value="ADD"
-				>
-			</p>
-		</form>
+		<!-- Button trigger modal -->
+		<button type="button" 
+				class="btn btn-primary mb-1 mt-4" 
+				data-bs-toggle="modal" 
+				data-bs-target="#addFriendModal">
+			add friend...
+		</button>
 
-		<div class="friendList">
-			<ul>
-				<li v-for="friend in friendList" :key="friend" >
+		<!-- Modal -->
+		<div class="modal fade" id="addFriendModal" tabindex="-1" aria-labelledby="addFriendModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="addFriendModalLabel">Add friend</h5>
+					<button id="closeModalButton" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					<div class="row justify-content-center">
+						<div class="col-lg-6">
+							<form class="needs-validation" novalidate v-on:submit.prevent="addFriend">
+								<div class="position-relative">
+									<label for="id" class="form-label">friend username </label>
+									<input
+										id="id"
+										class="form-control"
+										v-model="friendToAdd.friendUsername"
+										type="text"
+										name="id"
+										placeholder="Username..."
+										required
+									>
+									<div class="valid-tooltip" v-if="errorMsg === ''">
+										Looks good!
+									</div>
+								</div>
+								<div class="valid-feedback" v-if="errorMsg === ''">
+									Friend added!
+								</div>
+								<div id="validationServerUsernameFeedback" class="invalid-feedback" v-if="errorMsg !== ''" style="color: red">
+									{{errorMsg}}
+								</div>
+
+								<div id="idHelp" class="form-text" v-if="errorMsg !== ''" style="color: red">{{errorMsg}}</div>
+								
+								<div class="mb-4 mt-4 text-center">
+									<button
+										type="submit"
+										class="btn btn-primary"
+										value="ADD"
+										>Send friend request</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		</div>
+
+		<div class="container-xl">
+			<ul class="list-group mb-4 mt-4">
+				<li v-for="friend in friendList" :key="friend" class="list-group-item  d-flex justify-content-between">
 					{{friend.username}} ({{friend.firstname}}, {{friend.lastname}}) is {{friend.status}} {{this.userStatus}}
-					<button v-on:click="deleteFriend(friend.username)"> DELETE </button>
+					<button v-on:click="deleteFriend(friend.username)">
+						<span class="badge bg-primary rounded-pill">X</span>
+					</button>
 				</li>
 			</ul>
 		</div>
+
 	</div>
 </template>
 
@@ -63,7 +105,11 @@ export default defineComponent({
 		async addFriend() {
 			await http.post('/users/addfriend', this.friendToAdd)
 			.then(
-				response => { console.log("/users/addfriend success", response); this.getFriendList(); this.errorMsg = "" }
+				response => { console.log("/users/addfriend success", response);
+					this.getFriendList();
+					this.errorMsg = "";
+					(document.getElementById("closeModalButton")as any).click();
+				}
 			)
 			.catch(
 				error => { console.log(" /users/addfriend msg = ", error.response.data.error, "full error = ", error), this.errorMsg = error.response.data.error }
@@ -84,6 +130,7 @@ export default defineComponent({
 		friendListloop(sec: number) {
 			setInterval( () => {this.getFriendList}, sec * 1000);
 		}
+
 	},
 
 	created () {
