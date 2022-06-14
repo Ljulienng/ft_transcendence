ee1ef4235b80d51651999aa2afdc6db002344d95
Nguyen Julien <Ljulienng>
2022-06-14 21:13:31 +0200

    small fix

--- front/src/components/chat/Message.vue
+++ front/src/components/chat/Message.vue
@@ -1,67 +1,50 @@
 <template>
+  <div>
+    <div v-if="sender !== currentUser">
+      <p class="username">{{ sender }}</p>
 
-    <div>
-
-        <div v-if="sender !== currentUser">
-
-            <p class="username">{{sender}}</p>
-            
-            <div class="container-sm from">
-                {{content}}
-            </div>
-
-        </div>
-
-        <div v-if="sender === currentUser">
-
-            <p class="username">you</p>
-            
-            <div class="container to">
-                {{content}}
-            </div>
-
-        </div>
-
+      <div class="container-sm from">
+        {{ content }}
+      </div>
     </div>
 
+    <div v-if="sender === currentUser">
+      <p class="username">you</p>
+
+      <div class="container to">
+        {{ content }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
 
-    import { defineComponent } from "@vue/runtime-core";
-    import store from "../../store";
+export default defineComponent({
+  props: ["sender", "content", "currentUser"],
 
-    export default defineComponent({
-
-        props: ['sender', 'content', 'currentUser'],
-
-        data() {
-            return {
-            
-            };
-        },
-
-    });
-
+  data() {
+    return {};
+  },
+});
 </script>
 
 <style lang="scss" scoped>
+.username {
+  font-size: small;
+  margin: 0%;
+}
 
-    .username {
-        font-size:small;
-        margin: 0%;
-    }
+.from {
+  background-color: #c4c4c480;
+  border-radius: 20px;
+  padding: 10px;
+}
 
-    .from {
-        background-color: #c4c4c480;
-        border-radius: 20px;
-        padding: 10px;
-    }
-
-    .to {
-        background-color: #fff774;
-        border-radius: 20px;
-        padding: 10px;
-    }
-
+.to {
+  background-color: #fff774;
+  border-radius: 20px;
+  padding: 10px;
+}
 </style>
