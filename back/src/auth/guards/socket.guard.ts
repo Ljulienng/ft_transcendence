3639c2dfd3edaa80cfcc79df20c54558cf9ce065
Nguyen Julien <Ljulienng>
2022-06-22 19:29:50 +0200

    new double auth modal

--- back/src/auth/guards/socket.guard.ts
+++ back/src/auth/guards/socket.guard.ts
@@ -21,10 +21,16 @@ export class SocketGuard implements CanActivate {
 		
 		const user = await this.userService.findByUsername(username);
 		
-		if (!user)
-			throw new ForbiddenException('User doesn\'t exist')
-		if (payload['auth'] === false && user.twoFAEnabled === true)
-			throw new ForbiddenException('User need to authenticate')
+		if (!user) {
+			// throw new ForbiddenException('User doesn\'t exist')
+			console.log('User doesn\'t exist');
+			return false
+		}
+		if (payload['auth'] === false && user.twoFAEnabled === true) {
+			console.log('User need to authenticate');
+			return false
+			// throw new ForbiddenException('User need to authenticate')
+		}
 	// const decode = this.jwtService.decode(req.cookies.jwt)
 		return true;
 }
